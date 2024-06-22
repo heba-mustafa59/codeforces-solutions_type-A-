fae46fd07becb86b5702360adab26cfa06ce87d4
Heba_Mustafa🍒 <163648306+heba-mustafa59@users.noreply.github.com>
2024-06-22 17:25:01 +0300

    Add files via upload
    A. Phone Code codeforces solution

---
+++ nn.cpp
@@ -0,0 +1,23 @@
+﻿/* yildiz
+*/
+#include <iostream>
+#include <cmath>
+#include <vector>
+#include <string>
+#include <algorithm>
+using namespace std;
+
+int main() {
+    string str;
+    cin >> str;
+
+    size_t n = str.find('.');
+    str[n - 1] == '9' ? cout << "GOTO Vasilisa." << "\n" :
+        (str[n + 1] >= '5' ? str[n - 1] += 1 : 0,
+            str.erase(str.begin() + n, str.end()), cout << str << "\n");
+
+    return 0;
+}
+
+
+
