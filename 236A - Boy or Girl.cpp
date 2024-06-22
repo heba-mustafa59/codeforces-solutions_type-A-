919536c08c4ba0186d7e4fbbcbf1148527787a95
Heba_Mustafa🍒 <163648306+heba-mustafa59@users.noreply.github.com>
2024-06-22 17:33:50 +0300

    Add files via upload

---
+++ 236A - Boy or Girl.cpp
@@ -0,0 +1,26 @@
+﻿/* yildiz
+*/
+#include <stdio.h>
+#include <iostream>
+#include <cmath>
+#include <vector>
+#include <string>
+#include <algorithm>
+using namespace std;
+
+int main()
+{
+    string s;
+    cin >> s;
+    sort(s.begin(), s.end());
+    s.erase(unique(s.begin(), s.end()), s.end());
+    if (s.length() % 2 == 0)
+    {
+        cout << "CHAT WITH HER!" << endl;
+    }
+    else
+    {
+        cout << "IGNORE HIM!" << endl;
+    }
+    return 0;
+}
