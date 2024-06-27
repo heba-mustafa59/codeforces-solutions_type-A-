cb4f45da8c93745ec5c43485c584f1e731befea3
Heba 🍒 <163648306+heba-mustafa59@users.noreply.github.com>
2024-06-27 16:03:34 +0300

    194A - Exams

---
+++ 194A - Exams.cpp
@@ -0,0 +1,16 @@
+﻿
+/* 
+      yildiz
+*/
+#include <iostream>
+#include <vector>
+#include <cstdlib>
+#include <cmath>
+using namespace std;
+
+int main() {
+    int n, k;
+    cin >> n >> k;
+    cout << (k <= n * 3 ? n * 3 - k : 0) << endl;
+    return 0;
+}
