59d9d8a5cdbf2a5426f6557c6acf9140a50ad717
Heba 🍒 <163648306+heba-mustafa59@users.noreply.github.com>
2024-06-27 15:34:28 +0300

    74A - Room Leader

---
+++ 74A - Room Leader.cpp
@@ -0,0 +1,28 @@
+﻿
+/* 
+      yildiz
+*/
+#include <iostream>
+#include <vector>
+#include <cstdlib>
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+    int n, p, m, a, b, c, d, e;
+    int maxPoints(-2501);
+    string h, leader;
+    cin >> n;
+
+    while (n--) {
+        cin >> h >> p >> m >> a >> b >> c >> d >> e;
+        int points = p * 100 - m * 50 + a + b + c + d + e;
+        if (points > maxPoints) {
+            leader = h;
+            maxPoints = points;
+        }
+    }
+    cout << leader;
+    return 0;
+}
