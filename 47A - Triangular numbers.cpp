34a8672f2751f5efb52d7eda258a76d897987818
Heba 🍒 <163648306+heba-mustafa59@users.noreply.github.com>
2024-06-27 15:48:07 +0300

    47A - Triangular numbers

---
+++ 47A - Triangular numbers.cpp
@@ -0,0 +1,24 @@
+﻿
+/* 
+      yildiz
+*/
+#include <iostream>
+#include <vector>
+#include <cstdlib>
+#include <cmath>
+
+using namespace std;
+int main() {
+    int n = 0;
+    cin >> n;
+
+    int temp = sqrt(1 + 8 * n);
+    if (temp * temp == 1 + 8 * n) {
+        cout << "YES" << endl;
+    }
+    else {
+        cout << "NO" << endl;
+    }
+
+    return 0;
+}
