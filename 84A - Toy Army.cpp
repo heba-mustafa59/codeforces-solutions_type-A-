ba192805394e4f585ea87769f2ca6dfebb0ffae1
Heba 🍒 <163648306+heba-mustafa59@users.noreply.github.com>
2024-06-27 15:21:21 +0300

    84A - Toy Army

---
+++ 84A - Toy Army.cpp
@@ -0,0 +1,19 @@
+﻿
+/* 
+      yildiz
+*/
+#include <iostream>
+#include <vector>
+#include <cstdlib>
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main()
+{
+    int num;
+    cin >> num;
+    int n = num + num / 2;
+    cout << n << endl;
+    return 0;
+}
