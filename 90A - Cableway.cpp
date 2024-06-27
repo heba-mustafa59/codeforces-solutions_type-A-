da4c3d6ccdcffc5ad8fb15997a4e78514f28a492
Heba 🍒 <163648306+heba-mustafa59@users.noreply.github.com>
2024-06-27 15:16:42 +0300

    Add files via upload

---
+++ 90A - Cableway.cpp
@@ -0,0 +1,34 @@
+﻿
+/* 
+      yildiz
+*/
+#include <iostream>
+#include <vector>
+#include <cstdlib>
+#include <iostream>
+#include <vector>
+using namespace std;
+int main() {
+
+    int r = 0, g = 0, b = 0;
+    cin >> r >> g >> b;
+
+    r = (r + 1) / 2;
+    g = (g + 1) / 2;
+    b = (b + 1) / 2;
+
+    int output = 0;
+    if (b >= g && b >= r) {
+        output = 30 + 3 * b - 1;
+    }
+    else if (g >= r) {
+        output = 30 + 3 * g - 2;
+    }
+    else {
+        output = 30 + 3 * r - 3;
+    }
+
+    cout << output << endl;
+
+    return 0;
+}
