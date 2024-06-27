b3edb5f4a63f070d0991ca9c33d07fb9b3c87170
Heba 🍒 <163648306+heba-mustafa59@users.noreply.github.com>
2024-06-27 15:08:17 +0300

    Add files via upload

---
+++ 92A - Chips.cpp
@@ -0,0 +1,26 @@
+﻿
+/* 
+      yildiz
+*/
+#include <iostream>
+#include <vector>
+#include <cstdlib>
+#include <iostream>
+#include <vector>
+using namespace std;
+
+
+int main() {
+    int x, y;
+    cin >> x >> y;
+    y %= (x * (x + 1) / 2); 
+
+    for (int i = 1; i <= x; ++i) {
+        if (y < i) {
+            break;
+        }
+        y -= i;
+    }
+    cout << y << endl;
+    return 0;
+}
