c26e5a4b4a0a46c47d21259fcb2f3f706c9a607f
Heba 🍒 <163648306+heba-mustafa59@users.noreply.github.com>
2024-06-30 12:44:19 +0300

    Add files via upload

---
+++ A - Hit the Lottery.cpp
@@ -0,0 +1,37 @@
+﻿/*
+      yildiz
+*/
+#include <iostream>
+#include <string>
+#include<iomanip>
+#include <algorithm>
+#include <vector>
+using namespace std;
+int main()
+{
+    long long n;
+    cin >> n;
+    int c = 0, m = 0, b = 0, l = 0;
+    if (n >= 100) {
+        c += (n / 100);
+        n -= (c * 100);
+    }
+
+    if (n >= 20) {
+        m += (n / 20);
+        n -= (m * 20);
+
+    }
+    if (n >= 10) {
+        l += (n / 10);
+        n -= (l * 10);
+
+    }
+    if (n >= 5) {
+        b += (n / 5);
+        n -= (b * 5);
+    }
+    if (n >= 1)c += n;
+    cout << c + b + m + l;
+    return 0;
+}
