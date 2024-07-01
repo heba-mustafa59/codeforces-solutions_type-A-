5c1e2ceeb06cc04f6b0dbdbc667b06c2c4b1e200
Heba 🍒 <163648306+heba-mustafa59@users.noreply.github.com>
2024-07-01 23:17:19 +0300

    1783A - Make it Beautiful

---
+++ 1783A - Make it Beautiful.cpp
@@ -0,0 +1,33 @@
+﻿/*
+      yildiz
+*/
+#include <string>
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <vector>
+using namespace std;
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        vector<int> v(n);
+        for (int i = 0; i < n; i++) {
+            cin >> v[i];
+        }
+        swap(v[0], v[1]);
+        swap(v[n - 1], v[0]);
+
+        if (v[0] == v[1])
+            cout << "NO\n";
+        else {
+            cout << "YES\n";
+            for (int i = 0; i < n; i++) {
+                cout << v[i] << " ";
+            }
+            cout << "\n";
+        }
+
+    }
