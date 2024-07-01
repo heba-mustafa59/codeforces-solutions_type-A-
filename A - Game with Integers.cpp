982f11d6a109c2d8af2e64403c6cba458f7b6fc4
Heba 🍒 <163648306+heba-mustafa59@users.noreply.github.com>
2024-07-01 23:44:19 +0300

    A - Game with Integers

---
+++ A - Game with Integers.cpp
@@ -0,0 +1,32 @@
+﻿/*
+      yildiz
+*/
+#include <string>
+#include <iostream>
+#include <cmath>
+#include <algorithm>
+#include <vector>
+using namespace std;
+
+int main() {
+    long long t;
+    cin >> t;
+    while (t--) {
+        long long n;
+        cin >> n;
+        if (n % 3 == 0) {
+            cout << "Second" << "\n";
+
+
+        }
+        else if (n % 3 == 1) {
+            cout << "First" << "\n";
+
+        }
+        else {
+            cout << "First" << "\n";
+
+        }
+
+    }
+}
