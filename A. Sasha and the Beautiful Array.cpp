d63f2b732eea9bd6a86e478fb1ae113e45f9e1a7
Heba 🍒 <163648306+heba-mustafa59@users.noreply.github.com>
2024-07-01 11:22:56 +0300

    A. Sasha and the Beautiful Array

---
+++ A. Sasha and the Beautiful Array.cpp
@@ -0,0 +1,29 @@
+﻿/*
+      yildiz
+*/
+#include <iostream>
+#include <string>
+#include<iomanip>
+#include <algorithm>
+#include <vector>
+using namespace std;
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        vector<int> arr(n);
+        for (int i = 0; i < n; i++) {
+            cin >> arr[i];
+        }
+        sort(arr.begin(), arr.end());
+        int s = 0;
+        for (int i = 1; i < n; i++) {
+            s += (arr[i] - arr[i - 1]);
+        }
+        cout << s << "\n";
+    }
+    return 0;
+}
