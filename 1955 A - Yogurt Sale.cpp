1efaf082dd5c8940513729b58646c4dd28eb3d5a
Heba 🍒 <163648306+heba-mustafa59@users.noreply.github.com>
2024-07-02 22:04:02 +0300

    1955 A - Yogurt Sale

---
+++ 1955 A - Yogurt Sale.cpp
@@ -0,0 +1,30 @@
+﻿/*
+      yildiz
+*/
+#include <string>
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <vector>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n, a, b;
+        cin >> n >> a >> b;
+        if (2 * a <= b)
+            cout << n * a << "\n";
+        else
+        {
+            if (n & 1)
+                cout << n / 2 * b + a << "\n";
+            else
+                cout << n / 2 * b << "\n";
+        }
+    }
+    return 0;
+}
