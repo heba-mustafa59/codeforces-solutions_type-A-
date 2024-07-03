b60e349b93838567c762ec1e4166972e0f9167e4
Heba 🍒 <163648306+heba-mustafa59@users.noreply.github.com>
2024-07-03 21:45:31 +0300

    A - Odd Divisor

---
+++ A - Odd Divisor.cpp
@@ -0,0 +1,29 @@
+﻿/*
+      yildiz
+*/
+#include <string>
+#include <iostream>
+#include <cmath>
+#include <algorithm>
+#include <vector>
+using namespace std;
+
+int main() {
+    int t;
+    cin >> t;
+
+    while(t--) {
+        long long n;
+        cin >> n;
+
+        long long f = n & (n - 1);
+        if (f == 0) {
+            cout << "NO" << "\n";
+        }
+        else {
+            cout << "YES" << "\n";
+        }
+    }
+
+    return 0;
+}
