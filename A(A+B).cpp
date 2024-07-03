c9f06c895e42e5ecafca5769fcf0591955fc0d16
Heba 🍒 <163648306+heba-mustafa59@users.noreply.github.com>
2024-07-03 22:03:12 +0300

    A - A+B?

---
+++ A(A+B).cpp
@@ -0,0 +1,38 @@
+﻿/*
+      yildiz
+*/
+#include <string>
+#include <iostream>
+#include <cmath>
+#include <algorithm>
+#include <vector>
+using namespace std;
+int main() {
+    int t;
+    cin >> t;
+
+    while (t--) {
+        string s;
+        cin >> s;
+        int c = s[0] - '0';
+        char ch = s[1];
+        int y = s[2] - '0';
+
+        switch (ch) {
+        case '+':
+            cout << (y + c) << "\n";
+            break;
+        case '-':
+            cout << (y - c) << "\n";
+            break;
+        case '*':
+            cout << (y * c) << "\n";
+            break;
+        case '/':
+            cout << (y / c) << "\n";
+            break;
+        }
+    }
+
+    return 0;
+}
