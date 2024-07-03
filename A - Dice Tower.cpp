badf848d8af308753fe3a6b2c1c0006bec0a6bf1
Heba 🍒 <163648306+heba-mustafa59@users.noreply.github.com>
2024-07-03 07:39:51 +0300

    A - Dice Tower

---
+++ A - Dice Tower.cpp
@@ -0,0 +1,29 @@
+﻿/*
+      yildiz
+*/
+#include <string>
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <vector>
+using namespace std;
+int main()
+{
+    int n, x;
+    cin >> n >> x;
+
+    bool heba = true;
+    while (n--) {
+        int a, b;
+        cin >> a >> b;
+
+        if (7 - a == x || 7 - b == x || a == x || b == x) {
+            heba= false;
+        }
+    }
+
+    if (heba) cout << "YES" << "\n";
+    else cout << "NO" << "\n";
+
+    return 0;
+}
