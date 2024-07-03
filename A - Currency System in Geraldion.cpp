76796d5c9eb302a8559ee9f5c0f59c9b196a30f7
Heba 🍒 <163648306+heba-mustafa59@users.noreply.github.com>
2024-07-03 07:48:17 +0300

    A - Currency System in Geraldion

---
+++ A - Currency System in Geraldion.cpp
@@ -0,0 +1,24 @@
+﻿/*
+      yildiz
+*/
+#include <string>
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <vector>
+using namespace std;
+int main()
+{
+    int n;
+    cin >> n;
+    vector <int>v(n);
+    for (int i=0;i<n;i++) {
+        cin >> v[i];
+    }
+    sort(v.begin(), v.end());
+
+    if (v[0] == 1) cout << -1 << endl;
+    else cout << 1 << endl;
+
+    return 0;
+}
