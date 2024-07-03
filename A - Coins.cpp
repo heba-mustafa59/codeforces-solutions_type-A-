72a3315465e2624d6d7e874ed833235fd4691888
Heba 🍒 <163648306+heba-mustafa59@users.noreply.github.com>
2024-07-03 22:09:07 +0300

    A - Coins

---
+++ A - Coins.cpp
@@ -0,0 +1,27 @@
+﻿/*
+      yildiz
+*/
+#include <string>
+#include <iostream>
+#include <cmath>
+#include <algorithm>
+#include <vector>
+using namespace std;
+int main() {
+    int t;
+    cin >> t;
+
+    while(t--) {
+ 
+        long long a, b;
+        cin >> a >> b;
+
+        if (b % 2 == 0 && a % 2 != 0) {
+            cout << "NO" << "\n";
+        }
+        else {
+            cout << "YES" << "\n";
+        }
+    }
+    return 0;
+}
