89faad4f2006980e9bca8a8c3bbdc8f3ec5d1744
Heba 🍒 <163648306+heba-mustafa59@users.noreply.github.com>
2024-07-06 14:25:30 +0300

    A - Immobile Knight

---
+++ A - Immobile Knight.cpp
@@ -0,0 +1,20 @@
+﻿/*
+      yildiz
+*/
+#include <string>
+#include <iostream>
+#include <cmath>
+#include <algorithm>
+#include <vector>
+using namespace std;
+
+int main() {
+    int t,a,b;
+    cin >> t;
+    while (t--) {
+        cin >> a>> b;
+
+        cout << (int)ceil(a/ 2.0) << " " << (int)ceil(b/ 2.0) << "\n";
+    }
+  
+}
