c23e5026a349be251ddcd7b0b872df9ef5c7d026
Heba 🍒 <163648306+heba-mustafa59@users.noreply.github.com>
2024-07-07 14:11:57 +0300

    1968A - Maximize?

---
+++ 1968A - Maximize.cpp
@@ -0,0 +1,19 @@
+﻿/*
+      yildiz
+*/
+#include <string>
+#include <iostream>
+#include <cmath>
+#include <algorithm>
+#include <vector>
+using namespace std; 
+
+int main() {
+    int t, x;
+    cin >> t;
+    while (t--) {
+        cin >> x;
+        cout << x - 1 << "\n";
+    }
+    return 0;
+}
