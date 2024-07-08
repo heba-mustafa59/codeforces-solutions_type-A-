82848b15a081145af4b0d6295163129efbb598f7
Heba 🍒 <163648306+heba-mustafa59@users.noreply.github.com>
2024-07-08 15:50:57 +0300

    1937A - Shuffle Party

---
+++ 1937A - Shuffle Party.cpp
@@ -0,0 +1,26 @@
+﻿/*
+      yildiz
+*/
+#include <string>
+#include <iostream>
+#include <cmath>
+#include <algorithm>
+#include <vector>
+#include <unordered_map>
+using namespace std;
+
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        int x = 1;
+        while (x * 2 <= n) {
+            x *= 2;
+        }
+        cout << x << "\n";
+    }
+    return 0;
+}
