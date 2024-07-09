df7def3f46ada96647c07ba4ee735ea738d3f82e
Heba 🍒 <163648306+heba-mustafa59@users.noreply.github.com>
2024-07-09 20:46:03 +0300

    1919A - Wallet Exchange

---
+++ 1919A - Wallet Exchange.cpp
@@ -0,0 +1,27 @@
+﻿/*
+      yildiz
+*/
+#include <string>
+#include <iostream>
+#include <cmath>
+#include <algorithm>
+#include <vector>
+#include <unordered_map>
+using namespace std;
+
+
+int main() {
+    int t;
+    cin >> t;
+
+    while (t--) {
+        int a, b;
+        cin >> a >> b;
+        if ((a + b) & 1)
+            cout << "Alice" << "\n";
+        else
+            cout << "Bob" << "\n";
+    }
+
+return 0;
+}
