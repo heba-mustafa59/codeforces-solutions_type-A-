01fc7b30760f56c595eea4c9718b0f33e18b5d99
Heba 🍒 <163648306+heba-mustafa59@users.noreply.github.com>
2024-07-14 14:30:38 +0300

    A - Water Buying

---
+++ A - Water Buying.cpp
@@ -0,0 +1,37 @@
+﻿/*
+      yildiz
+*/
+#define ll  long long 
+#include <string>
+#include <iostream>
+#include <cmath>
+#include <algorithm>
+#include <vector>
+#include <unordered_map>
+#include <queue>
+#include <map>
+using namespace std;
+
+int main() {
+    int t;
+    cin >> t;
+
+    while (t--) {
+        ll n;
+        int a, b;
+        cin >> n >> a >> b;
+
+        ll r = 0;
+
+        if (a * 2 < b) {
+            r = a * n;
+        }
+        else {
+            r = (n / 2) * b + (n % 2) * a;
+        }
+
+        cout << r<< "\n";
+    }
+
+    return 0;
+}
