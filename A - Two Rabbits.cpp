5b0f70131323b4593bfc50559d1c4e93cf4b6626
Heba 🍒 <163648306+heba-mustafa59@users.noreply.github.com>
2024-07-14 14:21:16 +0300

    A - Two Rabbits

---
+++ A - Two Rabbits.cpp
@@ -0,0 +1,35 @@
+﻿/*
+      yildiz
+*/
+#include <string>
+#include <iostream>
+#include <cmath>
+#include <algorithm>
+#include <vector>
+#include <unordered_map>
+#include <queue>
+#include <map>
+using namespace std;
+
+
+int main() {
+    int n;
+    cin >> n;  
+
+    for (int i = 0; i < n; ++i) {
+        int x, y, a, b;
+        cin >> x >> y >> a >> b;  
+
+        int d = y - x;
+        int sum = a + b;
+
+        if (d % sum == 0) {   
+            cout << d / sum << "\n";
+        }
+        else {
+            cout << -1 << "\n";
+        }
+    }
+
+    return 0;
+}
