d074e68e2465ecbef63f260643e944090da9bf4f
Heba 🍒 <163648306+heba-mustafa59@users.noreply.github.com>
2024-07-15 10:59:43 +0300

    A - Kids Seating

---
+++ A - Kids Seating.cpp
@@ -0,0 +1,31 @@
+﻿/*
+      yildiz
+*/
+#define ll  long long 
+#include <string>
+#include <iostream>
+#include <cmath>
+#include <algorithm>
+#include <vector>
+#include <unordered_map>
+#include <queue>
+#include <map>
+using namespace std;
+
+
+int main() {
+    int t, n;
+    cin >> t;
+
+    while (t--) {
+        cin >> n; 
+        int x = 4 * n;
+        for (int i = 0; i < n; i++) {
+            cout << x << " ";
+            x -= 2;
+        }
+         cout << "\n";
+    }
+
+    return 0;
+}
